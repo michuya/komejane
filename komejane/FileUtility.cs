783b948640853162d880d254cbd807865808e49c
みちゅや <michuya328@gmail.com>
2016-06-21 12:08:36 +0900

    mime取得ユーティリティ追加

---
+++ komejane/FileUtility.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komejane
+{
+  class FileUtility
+  {
+    /* --------------------------------------------------------------------- */
+    #region FindMimeFromData
+    /* --------------------------------------------------------------------- */
+    private enum FMFD : uint
+    {
+      FMFD_DEFAULT = 0,
+      FMFD_URLASFILENAME = 1 << 0,
+      FMFD_ENABLEMIMESNIFFING = 1 << 1,
+      FMFD_IGNOREMIMETEXTPLAIN = 1 << 2,
+      FMFD_SERVERMIME = 1 << 3, // ?
+    }
+
+    [DllImport("urlmon")]
+    private static extern
+    uint FindMimeFromData(IntPtr pBC,
+                         IntPtr pwzUrl,
+                         byte[] buffer,
+                         int cbSize,
+                         [In, MarshalAs(UnmanagedType.LPWStr)] string pwzMimeProposed,
+                         FMFD dwMimeFlags,
+                         [MarshalAs(UnmanagedType.LPWStr)] out string ppwzMimeOut,
+                         uint dwReserved);
+
+    public static string FindMimeFromData(byte[] data, string mimeProposed = null)
+    {
+      const uint E_INVALIDARG = 0x80070057;
+      string mime;
+
+      var ret = FindMimeFromData(IntPtr.Zero, IntPtr.Zero, data, data.Length, mimeProposed, FMFD.FMFD_DEFAULT, out mime, 0);
+
+      if (ret == 0)
+        return mime;
+      else if (ret == E_INVALIDARG)
+        throw new ArgumentException();
+      else
+        return null;
+    }
+
+    public static string FindMimeFromStream(System.IO.StreamReader stream)
+    {
+      char[] readBuff = new char[256];
+      int readBytes = stream.ReadBlock(readBuff, 0, 256);
+
+      List<byte> buff = new List<byte>(readBytes);
+      foreach (char c in readBuff)
+      {
+        buff.AddRange(BitConverter.GetBytes(c));
+      }
+
+      return FindMimeFromData(buff.ToArray());
+    }
+
+    public static string FindMimeFromFile(string path)
+    {
+      System.IO.StreamReader stream = new System.IO.StreamReader(path);
+
+      string result = FindMimeFromStream(stream);
+
+      stream.Close();
+      stream.Dispose();
+
+      return result;
+    }
+    /* --------------------------------------------------------------------- */
+    #endregion
+    /* --------------------------------------------------------------------- */
+  }
+}
