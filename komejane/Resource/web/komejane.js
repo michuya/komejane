f5e908b63727ca4e33727da5f2eb43b5596a0bd6
みちゅや <michuya328@gmail.com>
2016-06-23 14:58:03 +0900

    リソースにHTML関連のスケルトンを追加

---
+++ komejane/Resource/web/komejane.js
@@ -0,0 +1,13 @@
+﻿axios
+  .get('/api/wsInfo')
+  .then((res) => {
+      const wsUri = res.data.websocket.streamUri || "/stream";
+      var socket = io(wsUri);
+      socket.on('news', function (data) {
+          console.log(data);
+          socket.emit('my other event', { my: 'data' });
+      });
+  })
+  .catch((error) => {
+
+  })
