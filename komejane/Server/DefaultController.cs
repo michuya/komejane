6e6ad879a9f79a9a9bb7b1f8466b867aa1670249
みちゅや <michuya328@gmail.com>
2016-06-30 05:12:42 +0900

    コントローラ関連を追加

---
+++ komejane/Server/DefaultController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Komejane.Server
+{
+  class DefaultController : BasicController
+  {
+    string WebRootDirectory { get; set; }
+    string WebIndex { get; set; }
+
+    /// <summary>
+    /// 拡張子ごとのMIME辞書。辞書登録する拡張子には「.」を含めない事。
+    /// </summary>
+    public Dictionary<string, string> MimeDictionary { get; set; }
+
+    public DefaultController(string webroot, string webindex) : base()
+    {
+      WebRootDirectory = webroot;
+      WebIndex = webindex;
+    }
+
+    public override void index(HttpListenerRequest req, HttpListenerResponse res)
+    {
+      // リクエストのローカルパスを組み立て
+      string requestURI = Path.Combine(WebRootDirectory, req.Url.LocalPath);
+      Logger.Debug("RequestURI: " + requestURI);
+
+      // ディレクトリだった場合はインデックスのファイルを追加
+      if (Directory.Exists(requestURI))
+      {
+        requestURI += "\\" + WebIndex;
+
+        Logger.Debug("RequestURI(Append Index): " + requestURI);
+      }
+
+      // ファイルが存在する場合はファイルを返す
+      try
+      {
+        if (!FileResponse(res, requestURI))
+        {
+          // FileResponseが失敗した場合は404
+          res.StatusCode = 404;
+        }
+      }
+      catch (Exception)
+      {
+        // FileResponseが例外だした場合も404
+        res.StatusCode = 404;
+      }
+
+      // ストリームの最後の処理はBasicControllerに任せる
+      base.index(req, res);
+    }
+
+    protected bool FileResponse(HttpListenerResponse res, string filepath)
+    {
+      // 指定したファイルがファイルじゃなかったら失敗
+      if (File.Exists(filepath)) return false;
+
+      try
+      {
+        FileInfo info = new FileInfo(filepath);
+
+        // MimeTypeを取得
+        res.ContentType = GetMimeType(filepath);
+
+        // ファイルサイズを設定
+        res.ContentLength64 = info.Length;
+
+        // ファイル読み込み用ストリームから直接レスポンス用のストリームへコピー
+        FileStream fs = new FileStream(filepath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+        fs.CopyTo(res.OutputStream);
+
+        return true;
+      }
+      catch (Exception ex)
+      {
+        Logger.Info(ex.ToString());
+
+        // 例外発生しても失敗。ここで出るのだいたいIOExceptionだし。
+        return false;
+      }
+    }
+
+    protected string GetMimeType(string filepath)
+    {
+      FileInfo info = new FileInfo(filepath);
+
+      string ext = info.Extension.ToLower().TrimStart(".".ToCharArray());
+
+      // デフォルト値はoctet-streamにしておく
+      string mimeType = "application/octet-stream";
+
+      try
+      {
+        if (MimeDictionary.ContainsKey(ext))
+        {
+          // 拡張子MIME辞書から取得
+          mimeType = MimeDictionary[ext];
+          Logger.Trace("MimeType(fromExtention): " + mimeType);
+        }
+        else
+        {
+          // ファイル内容からMIMEを推定
+          mimeType = FileUtility.FindMimeFromFile(filepath);
+          Logger.Trace("MimeType(fromFile): " + mimeType);
+        }
+      }
+      catch (Exception ex)
+      {
+        Logger.Info(ex.ToString());
+
+        // 例外が発生した場合は判定失敗とみなしてoctet-streamを返す
+        return "application/octet-stream";
+      }
+
+      return mimeType;
+    }
+  }
+}
---
+++ komejane/Server/RouterController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Reflection;
+
+namespace Komejane.Server
+{
+  public class ControllerWrapper
+  {
+    IRouterController controller = null;
+
+
+    public ControllerWrapper(string className, string @namespace = null)
+    {
+      CreateInstance(className, @namespace);
+    }
+
+    public void CreateInstance(string className, string @namespace = null, string asmName = null)
+    {
+      string @class = string.Format("{0}{2}{1}",
+        @namespace,
+        className,
+        (string.IsNullOrWhiteSpace(@namespace)) ? "" : "."
+      );
+      
+      lock (controller)
+      {
+        // アセンブリが指定されてた場合はアセンブリをロードしてインスタンスを生成する
+        if (string.IsNullOrWhiteSpace(asmName))
+        {
+          Assembly asm = Assembly.LoadFrom(asmName);
+
+          controller = (IRouterController)asm.CreateInstance(@class);
+        }
+        else
+        {
+          // Typeを取得する
+          Type t = Type.GetType(@class);
+
+          controller = (IRouterController)Activator.CreateInstance(t);
+        }
+      }
+    }
+
+    public void CallMethod(string method, HttpListenerRequest req, HttpListenerResponse res)
+    {
+      // コントローラが未設定の場合は処理しない
+      if (controller == null) return;
+
+      lock (controller)
+      {
+        // インスタンスのタイプを取得
+        Type t = controller.GetType();
+
+        // メソッド情報を取得
+        System.Reflection.MethodInfo mi = t.GetMethod(method);
+
+        // メソッドを呼び出す
+        mi.Invoke(controller, new object[] { req, res });
+      }
+    }
+  }
+
+  public interface IRouterController
+  {
+    void index(HttpListenerRequest req, HttpListenerResponse res);
+  }
+
+  public abstract class BasicController : IRouterController
+  {
+    public BasicController()
+    {
+
+    }
+
+    public virtual void index(HttpListenerRequest req, HttpListenerResponse res)
+    {
+      // アクセスログ
+      Logger.Info(req.RemoteEndPoint.Address + " \"" + req.HttpMethod + " " + req.RawUrl + " HTTP/" + req.ProtocolVersion + "\" " + res.StatusCode + " " + res.ContentLength64 + " \"" + req.UrlReferrer + "\" \"" + req.UserAgent + "\"");
+
+      // ストリームを閉じる
+      res.Close();
+    }
+  }
+}
