fc323fc5a1a6b60866c134bc53ba71ee8efb9fd7
みちゅや <michuya328@gmail.com>
2016-07-10 02:53:01 +0900

    Webスクリプトを更新

--- komejane/Resource/web/komejane.js
+++ komejane/Resource/web/komejane.js
@@ -1,12 +1,40 @@
-﻿axios
+﻿var ws = null;
+
+axios
   .get('/api/wsInfo')
   .then((res) => {
-      const wsUri = res.data.websocket.streamUri || "/stream";
-      var socket = io(wsUri);
-      socket.on('news', function (data) {
-          console.log(data);
-          socket.emit('my other event', { my: 'data' });
-      });
+    const wsUri = res.data.websocket.streamUri || "/stream";
+    if (ws == null) {
+      ws = new WebSocket("ws://" + location.host + wsUri);//, ['soap', 'xmpp']);
+      var pingInterval = null;
+
+      ws.onopen = () => {
+        ws.send("connect komejane client");
+        pingInterval = setInterval(() => {
+          console.log("ping");
+          ws.send("/ping");
+        }, 1000)
+      }
+
+      ws.onmessage = (msg) => {
+        if (msg.type == 'message') {
+          switch (msg.data) {
+            case "/pong":
+              console.log("pong☆");
+              break;
+          }
+        }
+      }
+
+      ws.onerror = (err) => {
+        console.log(err);
+      }
+
+      ws.onclose = () => {
+        if (pingInterval != null)
+          clearInterval(pingInterval);
+      }
+    }
   })
   .catch((error) => {
 
